b325dfb5a5de193ded1cc3d61a20841728a99058
Guillaume Fortin <xguillaumex@hotmail.com>
2014-01-23 00:37:19 -0500

    basic testing around bootstrap

---
+++ application/views/pages/home.php
@@ -0,0 +1,16 @@
+<div class="container">
+	<div class="home">
+		<h1>YouDeliberate</h1>
+
+		<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-search"></span></a>
+		<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-fire"></span></a>
+		<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-up"></span></a>
+		<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-down"></span></a>
+
+		<div class="jumbotron">
+			test
+		</div>
+
+		<a href="#" class="btn btn-warning btn-lg btn-feedback">Feedback</a>
+	</div>
+</div>
