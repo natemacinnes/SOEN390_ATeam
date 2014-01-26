dc37ca217e7af07f5596fef1f822cea43380ccf4
Guillaume Fortin <xguillaumex@hotmail.com>
2014-01-26 17:50:05 -0500

    alternative buttons home

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,17 +5,32 @@
 			<h3>Should GMO foods be labelled?</h3>
 			
 			
-			
 			<div class="sort-container float-left">
-				<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-certificate"></span> New</a>
-				<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-fire"></span> Popular</a>
-				<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-up"></span> Agreed</a>
-				<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-down"></span> Disagreed</a>
+				<div class="btn-group">
+					<a href="#" class="btn btn-default disabled" role="button">Sort by: </a>
+					<a href="#" title="New narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-certificate"></span> New</a>
+					<a href="#" title="Popular narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-fire"></span> Popular</a>
+					<a href="#" title="Agreed narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-up"></span> Agreed</a>
+					<a href="#" title="Disagreed narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-down"></span> Disagreed</a>
+				</div>
 			</div>
 			<div class="filter-container float-right">
-				<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-minus"></span> EN</a>
-				<a href="#" class="btn btn-default" role="button"><span class="glyphicon glyphicon-minus"></span> FR</a>
+				<div class="btn-group">
+					<a href="#" class="btn btn-default disabled" role="button">Filter by: </a>
+					<a href="#" title="English" class="btn btn-default" role="button"><span class="glyphicon glyphicon-minus"></span> EN</a>
+					<a href="#" title="French" class="btn btn-default" role="button"><span class="glyphicon glyphicon-minus"></span> FR</a>
+				</div>
 			</div>
+			<!--<div class="sort-container float-left">
+				<a href="#" title="New narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-certificate"></span> New</a>
+				<a href="#" title="Popular narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-fire"></span> Popular</a>
+				<a href="#" title="Agreed narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-up"></span> Agreed</a>
+				<a href="#" title="Disagreed narratives" class="btn btn-default" role="button"><span class="glyphicon glyphicon-thumbs-down"></span> Disagreed</a>
+			</div>
+			<div class="filter-container float-right">
+				<a href="#" title="English" class="btn btn-default" role="button"><span class="glyphicon glyphicon-minus"></span> EN</a>
+				<a href="#" title="French" class="btn btn-default" role="button"><span class="glyphicon glyphicon-minus"></span> FR</a>
+			</div>-->
 			<div class="clear"></div>
 			
 			<div id="bubble-container" class="top-margin">
@@ -24,8 +39,8 @@
 
 	</div>
 <div class="help-container">
-	<a href="#" class="btn btn-info btn-lg btn-help">Help?</a>
-	<a href="#" class="btn btn-warning btn-lg btn-feedback">Feedback</a>
+	<a href="#" title="Need help?" class="btn btn-info btn-lg btn-help">Help?</a>
+	<a href="#" title="Give us your feedback" class="btn btn-warning btn-lg btn-feedback">Feedback</a>
 </div>
 </div>
 
