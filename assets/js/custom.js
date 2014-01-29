53073a18f2a5a9f20e854795b7550caa10a72158
Stewart Adam <s.adam@diffingo.com>
2014-01-28 20:10:14 -0500

    Fix load of homepage with no sort ordring specified

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,20 @@
 jQuery(document).ready(function() {
-	loadBubbles();
+  loadBubbles();
 
-	// Toggle buttons for navigation links
-	jQuery('.btn-group a').click(function() {
-		jQuery(this).toggleClass('active');
-		var sortby = jQuery(this).attr('href').substring(1);
-		loadBubbles(sortby);
-	});
+  // Toggle buttons for navigation links
+  jQuery('.btn-group a').click(function() {
+    jQuery(this).toggleClass('active');
+    var sortby = jQuery(this).attr('href').substring(1);
+    loadBubbles(sortby);
+  });
 });
 
 function bubbleMouseIn(bubble) {
-	console.log('Mouse in: ' + this);
+  console.log('Mouse in: ' + this);
 }
 
 function bubbleMouseOut(bubble) {
-	console.log('Mouse out: ' + this);
+  console.log('Mouse out: ' + this);
 }
 
 // Returns a flattened hierarchy containing all leaf nodes under the root.
@@ -22,8 +22,8 @@ function classes(root) {
   var classes = [];
 
   function recurse(name, node) {
-	if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-	else classes.push({packageName: name, className: node.name, value: node.size});
+  if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+  else classes.push({packageName: name, className: node.name, value: node.size});
   }
 
   recurse(null, root);
@@ -31,41 +31,49 @@ function classes(root) {
 }
 
 function loadBubbles(sortBy) {
-		var diameter = (document.getElementById("bubble-container").offsetWidth)/2
-			format = d3.format(",d"),
-			color = d3.scale.category20c();
+  var diameter = (document.getElementById("bubble-container").offsetWidth)/2
+    format = d3.format(",d"),
+    color = d3.scale.category20c();
 
-		var bubble = d3.layout.pack()
-			.sort(null)
-			.size([diameter, diameter])
-			.padding(1.5);
+  var bubble = d3.layout.pack()
+    .sort(null)
+    .size([diameter, diameter])
+    .padding(1.5);
 
-		var svg = d3.select(".test").html('').append("svg")
-			.attr("width", diameter)
-			.attr("height", diameter)
-			.attr("class", "bubble");
+  var svg = d3.select(".test").html('').append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .attr("class", "bubble");
 
-		d3.json(yd_settings.site_url + "ajax/bubbles" + '/' + sortBy, function(error, root) {
-		  var node = svg.selectAll(".node")
-			  .data(bubble.nodes(classes(root))
-			  .filter(function(d) { return !d.children; }))
-			.enter().append("g")
-			  .attr("class", "node")
-			  .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-		  node.append("title")
-			  .text(function(d) { return d.className + ": " + format(d.value); });
+  // sortBy may be undefined. If so, don't call ajax/bubbles/undefined -_-
+  var url = yd_settings.site_url + "ajax/bubbles";
+  if (typeof(sortBy) !== 'undefined') {
+    url += '/' + sortBy;
+  }
 
-		  node.append("circle")
-			  .attr("r", function(d) { return d.r; })
-			  .style("fill", function(d) { return color(d.packageName); });
+  // Create the SVG bubble structure
+  d3.json(url, function(error, root) {
+    var node = svg.selectAll(".node")
+      .data(bubble.nodes(classes(root))
+      .filter(function(d) { return !d.children; }))
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-		  /*node.append("text")
-			  .attr("dy", ".3em")
-			  .style("text-anchor", "middle")
-			  .text(function(d) { return d.className.substring(0, d.r / 3); });*/
+    node.append("title")
+      .text(function(d) { return d.className + ": " + format(d.value); });
 
-	    d3.select(self.frameElement).style("height", diameter + "px");
-	    jQuery('svg.bubble .node').hover(bubbleMouseIn, bubbleMouseOut);
-  	});
-	}
+    node.append("circle")
+      .attr("r", function(d) { return d.r; })
+      .style("fill", function(d) { return color(d.packageName); });
+
+    /*node.append("text")
+      .attr("dy", ".3em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.className.substring(0, d.r / 3); });*/
+
+    d3.select(self.frameElement).style("height", diameter + "px");
+    jQuery('svg.bubble .node').hover(bubbleMouseIn, bubbleMouseOut);
+  });
+}
