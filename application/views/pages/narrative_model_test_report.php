b2044aadbc39103b96acc76be2bb60641ec56960
Jonathan Bobak <jon_bobak89@hotmail.com>
2014-01-30 23:27:18 -0500

    Unit Testing for narrative_model process_narrative function

---
+++ application/views/pages/narrative_model_test_report.php
@@ -0,0 +1,30 @@
+<?php
+
+echo $title;
+
+
+echo $narrativeModelMessage1;
+
+echo $narrativeModelError1;
+
+
+echo $narrativeModelMessage;
+
+echo $narrativeModelError;
+
+
+
+echo $narrativeModelNoError1;
+
+echo $narrativeModelNoMessage1;
+
+
+
+echo $narrativeModelNoError;
+
+echo $narrativeModelNoMessage;
+
+
+
+
+?>
