aec42c51b2052b582b706a8249aa154f6be4fd64
Stewart Adam <s.adam@diffingo.com>
2014-02-01 22:31:21 -0500

    Print, not return, values from a the ajax controller

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -49,34 +49,26 @@ class Ajax extends MY_Controller {
     $data['children'] = $groupings;
 
     print json_encode($data);
-	return $data;
   }
 
   public function audioImage($narrative_id, $time) {
     $current_time = floatval($time);
     $path = "./uploads/$narrative_id/AudioTimes.xml";
-	$return = "";
+    $return = "";
     if (file_exists($path) && $xml = simplexml_load_file($path)) {
       $timeNarrative =0;
-      if ($current_time <= floatval($xml->Narrative[$timeNarrative]->End))
-      {
-		$return = base_url() . 'uploads/' . $narrative_id . '/' . $xml->Narrative[$timeNarrative]->Image;
-        //echo $return;
-		return $return;
+      if ($current_time <= floatval($xml->Narrative[$timeNarrative]->End)) {
+        print base_url() . 'uploads/' . $narrative_id . '/' . $xml->Narrative[$timeNarrative]->Image;
       }
-      else
-      {
+      else {
         while($current_time > floatval($xml->Narrative[$timeNarrative]->End)){
           $timeNarrative +=1;
         }
-		$return = base_url() . 'uploads/' . $narrative_id . '/' .  $xml->Narrative[$timeNarrative]->Image;
-        //echo $return;
-		return $return;
+        print base_url() . 'uploads/' . $narrative_id . '/' .  $xml->Narrative[$timeNarrative]->Image;
       }
     }
     else {
-      print '';
-      return $return;
+      print $return;
     }
   }
 }
