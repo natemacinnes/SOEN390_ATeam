9b874c8cf8f70dcff1b4b7ca8776bbcd5094e385
Guillaume Fortin <xguillaumex@hotmail.com>
2014-02-04 22:15:44 -0500

    comments template

---
+++ application/views/pages/comments.php
@@ -0,0 +1,74 @@
+<div class="container">
+	<div class="page-header">
+    	<h1>Comments <small></small></h1>
+  	</div>
+
+  	<div class="filler float-left right-margin">
+  	</div>
+
+
+  	<div class="comments-container float-left">
+  		<div class="comments-wrapper">
+  			<div class="comment">
+  				<textarea class="form-control" rows="3" placeholder="Enter your comment..."></textarea>
+  				<a href="#" class="btn btn-primary btn-sm top-margin float-right" role="button">Post</a>
+  				<div class="clear"></div>
+  			</div>
+  			<div class="comment">
+  				<a class="report2" href="#"><span class="glyphicon glyphicon-flag"></span></a>
+  				<div class="quote" href="#">
+  					<p>"Test"</p>
+  				</div>
+  				<p>Test</p>
+  				<a class="reply" href="#">Reply</a>
+  			</div>
+  			<div class="comment">
+  				<a class="report" href="#"><span class="glyphicon glyphicon-flag"></span></a>
+  				<p>
+  					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique 
+  					ante eget adipiscing euismod. Etiam cursus aliquam ultricies. Praesent 
+  					pellentesque elementum nisi Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique ante eget adipiscing euismod. Etiam cursus aliquam ultricies. Praesent pellentesque elementum nisi
+  				</p>
+  				<a class="reply" href="#">Reply</a>
+  			</div>
+  			<div class="comment">
+  				<a class="report" href="#"><span class="glyphicon glyphicon-remove"></span></a>
+  				<div class="quote" href="#">
+  					<p>"Test"</p>
+  				</div>
+  				<textarea class="form-control" rows="3" placeholder="Enter your reply..."></textarea>
+  				<a href="#" class="btn btn-primary btn-sm top-margin float-right" role="button">Reply</a>
+  				<div class="clear"></div>
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  			<div class="comment">
+
+  			</div>
+  		</div>
+  	</div>
+
+  	<div class="clear"></div>
+</div>
