68256eec7ca2278ea584f84ce1babc92424368e8
Stewart Adam <s.adam@diffingo.com>
2014-02-17 21:29:54 -0500

    Fix merge conflict while reverting 9a831936c147971424c0814c390beccb6a35ee93

---
+++ application/controllers/pages.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Used for providing static/view-only pages.
+ */
+class Pages extends YD_Controller
+{
+	/**
+	 * Constructor: initialize required libraries.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+   * The default method called, if none is provided.
+   */
+	public function index()
+	{
+		$this->view_wrapper('pages/index');
+	}
+}
