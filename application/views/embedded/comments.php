575a03ae09af304e0e2120afc792412b6a7f291a
Massimo Catanzariti <mcatanzariti89@gmail.com>
2014-02-19 16:57:20 -0500

    Post button for commenting

--- application/views/embedded/comments.php
+++ application/views/embedded/comments.php
@@ -3,7 +3,7 @@
 	<div class="comments-wrapper">
 		<div class="comment">
 			<textarea class="form-control" rows="3" placeholder="Enter your comment..."></textarea>
-			<a href="#" class="btn btn-primary btn-sm top-margin float-right" role="button">Post</a>
+			<a href="#" class="btn btn-primary btn-sm top-margin float-right" id="<?php echo $narrative_id; ?>" role="button">Post</a>
 			<div class="clear"></div>
 		</div>
 	</div>
@@ -18,3 +18,19 @@
 		}
 	?>
 </div>
+<script type="text/javascript">
+	jQuery('.btn btn-primary btn-sm top-margin float-right').click(function() 
+	{
+		var narrative_id = jQuery('.btn btn-primary btn-sm top-margin float-right').attr('id');
+		var currentDate = new Date();
+		var dateCreated = currentDate.getFullYear() + "-"
+					+ (currentDate.getMonth() + 1) + "-"
+					+ currentDate.getDate() + " " +
+					+ currentDate.getHours() + ":"  
+					+ currentDate.getMinutes() + ":" 
+					+ currentDate.getSeconds();
+		var text = jQuery('.form-control').val();
+		var url = yd_settings.site_url + "comments/post_comment/" + narrative_id + "/NULL/" + dateCreated + "/" + text;
+		$.get(url, function() { alert( "Comment was added to database. Sample: " + text); });
+	}
+</script>
