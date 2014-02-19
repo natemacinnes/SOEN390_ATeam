29d24bf93f18b6af8ec05832770ea11d98518212
Massimo Catanzariti <mcatanzariti89@gmail.com>
2014-02-19 18:49:41 -0500

    comments check for empty posts now

--- application/views/embedded/comments.php
+++ application/views/embedded/comments.php
@@ -1,4 +1,7 @@
-
+<head>
+	<script src="SOEN390_ATeam/assets/js/jquery.min.js"></script>	
+</head>
+<body>
 <div class="comments-container float-left">
 	<div class="comments-wrapper">
 		<div class="comment">
@@ -11,7 +14,7 @@
 		if($comments == NULL)
 		{
 			echo "<div class='comment'>
-					<p id='defaut'>Comments show here</p>
+					<p id='default'>Comments show here</p>
 				  </div>";
 		}
 		foreach($comments as $com)
@@ -24,21 +27,31 @@
 		}
 	?>
 </div>
+</body>
 <script type="text/javascript">
-	jQuery('.btn btn-primary btn-sm top-margin float-right').click(function() 
+	jQuery(".btn.btn-primary.btn-sm.top-margin.float-right").click(function() 
 	{
-		document.getElementFromId('default').src = "Post Button Pressed";
-		var narrative_id = jQuery('.btn btn-primary btn-sm top-margin float-right').attr('id');
-		var currentDate = new Date();
-		var dateCreated = currentDate.getFullYear() + "-"
-					+ (currentDate.getMonth() + 1) + "-"
-					+ currentDate.getDate() + " " +
-					+ currentDate.getHours() + ":"  
-					+ currentDate.getMinutes() + ":" 
-					+ currentDate.getSeconds();
 		var text = jQuery('.form-control').val();
-		var url = yd_settings.site_url + "comments/post_comment/" + narrative_id + "/NULL/" + dateCreated + "/" + text;
-		$.get(url, function() { alert( "Comment was added to database. Sample: " + text); })
-			.fail(function() { alert( "Error Comment was not Added" ); });
-	}
+		
+		//Verify that the comments section is not Empty
+		if(text != "")
+		{
+			jQuery("#default").text("Posted");
+			var narrative_id = $(this).attr('id');
+			var currentDate = new Date();
+			var dateCreated = currentDate.getFullYear() + "-"
+						+ (currentDate.getMonth() + 1) + "-"
+						+ currentDate.getDate() + " " +
+						+ currentDate.getHours() + ":"  
+						+ currentDate.getMinutes() + ":" 
+						+ currentDate.getSeconds();
+			var url = yd_settings.site_url + "comments/post_comment/" + narrative_id + "/NULL/" + dateCreated + "/" + text;
+			$.get(url, function() { alert( "Comment was added to database. Sample: " + text); })
+				.fail(function() { alert( "Error Comment was not Added" ); });
+		}
+		else
+		{
+			alert("Error: No text was typed in the comments. Cannot post an empty comment.");
+		}
+	});
 </script>
