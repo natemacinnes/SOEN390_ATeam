db2acdc9362f3261a77e39348fa388d3fe441f30
gfortin13 <xguillaumex@hotmail.com>
2014-02-20 00:16:15 -0800

    fixed merge conflict with jad for master merge

--- application/views/admin/narratives/list.php
+++ application/views/admin/narratives/list.php
@@ -1,86 +1,88 @@
 <div class="container fixed-margin">
-  <div class="page-header">
-    <h1>Narratives <small>View All</small></h1>
-  </div>
+	<div class="page-header">
+		<h1>Narratives <small>View All</small></h1>
+	</div>
 
-  <!--<div class="dropdown dropdown-margin2 float-left">
-    <button class="btn dropdown-toggle " type="button" id="dropdownMenu1" data-toggle="dropdown">
-      Filter By ... <span class="caret"></span>
-    </button>
-    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Published</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Unpublished</a></li>
-    </ul>
-  </div>-->
-  
-  <?php echo form_open('admin/batchAction'); ?>
-  <ul class="pagination float-left">
-    <li><?php echo form_submit('delete', 'Delete', "class='btn btn-default'"); ?></li>
-    <li><?php echo form_submit('publish', 'Publish', "class='btn btn-default'"); ?></li>
-    <li><?php echo form_submit('unpublish', 'Unpublish', "class='btn btn-default'"); ?></li>
-  </ul>
-  <ul class="pagination float-right">
-    <li><a href="#">&laquo;</a></li>
-    <li><a href="#">1</a></li>
-    <li><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-    <li><a href="#">&raquo;</a></li>
-  </ul>
-  <div class="clear"></div>
+	<!--<div class="dropdown dropdown-margin2 float-left">
+		<button class="btn dropdown-toggle " type="button" id="dropdownMenu1" data-toggle="dropdown">
+			Filter By ... <span class="caret"></span>
+		</button>
+		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Published</a></li>
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Unpublished</a></li>
+		</ul>
+	</div>-->
+	
+	<!-- FIXME needs work for bulk actions and no form close -->
+	<?php echo form_open('admin/batchAction'); ?>
+	<ul class="pagination float-left">
+		<li><?php echo form_submit('delete', 'Delete', "class='btn btn-default'"); ?></li>
+		<li><?php echo form_submit('publish', 'Publish', "class='btn btn-default'"); ?></li>
+		<li><?php echo form_submit('unpublish', 'Unpublish', "class='btn btn-default'"); ?></li>
+	</ul>
 
-  <table class="table table-hover">
-    <thead>
-      <tr>
-		<th></th>
-        <th><a href="#" class="sort-btn active desc">ID</a></th>
-        <th><a href="#" class="sort-btn">Length</a></th>
-        <th><a href="#" class="sort-btn">Language</a></th>
-        <th><a href="#" class="sort-btn">Created</a></th>
-        <th><a href="#" class="sort-btn">Uploaded</a></th>
-        <th><a href="#" class="sort-btn">Flags</a></th>
-        <th><a href="#" class="sort-btn">Status</a></th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($narratives as $narrative): ?>
-        <tr>
-		  <td><?php echo form_checkbox('narratives[]', $narrative['narrative_id'], FALSE); ?>
-          <td><?php print $narrative['narrative_id']; ?></td>
-          <td><?php printf('%d', $narrative['audio_length']/60); ?>:<?php printf('%02d', $narrative['audio_length']%60); ?></td>
-          <td><?php print $narrative['language']; ?></td>
-          <td><?php print $narrative['created']; ?></td>
-          <td><?php print $narrative['uploaded']; ?></td>
-          <td><?php echo anchor('admin/narratives/' . $narrative['narrative_id'] . '/review', $narrative['flags']); ?></td>
-          <td><?php if($narrative['status'] == 1) echo 'Published'; else echo 'Not Published'; ?></td>
-          <td>
-          <?php echo anchor('admin/narratives/' . $narrative['narrative_id'] . '/review', '<span class="glyphicon glyphicon-eye-open"></span>', 'title="Review" class="btn btn-default btn-xs" role="button"'); ?>
-            <a href="<?php echo site_url('admin/comments/' . $narrative['narrative_id']); ?>" title="View Comments" class="btn btn-default btn-xs" role="button"><span class="glyphicon glyphicon-comment"></a>
-            <?php echo anchor('admin/narratives/' . $narrative['narrative_id'], '<span class="glyphicon glyphicon-pencil"></span>', 'title="Edit" class="btn btn-default btn-xs" role="button"'); ?>
-            <a href="#" title="Delete" class="btn btn-default btn-xs" role="button"><span class="glyphicon glyphicon-remove"></a>
-          </td>
-        </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
-  
-  <?php echo form_open('admin/batchAction'); ?>
-  <ul class="pagination float-left">
-    <li><?php echo form_submit('delete', 'Delete', "class='btn btn-default'"); ?></li>
-    <li><?php echo form_submit('publish', 'Publish', "class='btn btn-default'"); ?></li>
-    <li><?php echo form_submit('unpublish', 'Unpublish', "class='btn btn-default'"); ?></li>
-  </ul>
-  <ul class="pagination float-right">
-    <li><a href="#">&laquo;</a></li>
-    <li><a href="#">1</a></li>
-    <li><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-    <li><a href="#">&raquo;</a></li>
-  </ul>
-  <div class="clear"></div>
+	<ul class="pagination float-right">
+		<li><a href="#">&laquo;</a></li>
+		<li><a href="#">1</a></li>
+		<li><a href="#">2</a></li>
+		<li><a href="#">3</a></li>
+		<li><a href="#">4</a></li>
+		<li><a href="#">5</a></li>
+		<li><a href="#">&raquo;</a></li>
+	</ul>
+	<div class="clear"></div>
+
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th><a href="#" class="sort-btn active desc">ID</a></th>
+				<th><a href="#" class="sort-btn">Length</a></th>
+				<th><a href="#" class="sort-btn">Language</a></th>
+				<th><a href="#" class="sort-btn">Created</a></th>
+				<th><a href="#" class="sort-btn">Uploaded</a></th>
+				<th><a href="#" class="sort-btn">Flags</a></th>
+				<th><a href="#" class="sort-btn">Status</a></th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($narratives as $narrative): ?>
+				<tr>
+			<td><?php echo form_checkbox('narratives[]', $narrative['narrative_id'], FALSE); ?>
+					<td><?php print $narrative['narrative_id']; ?></td>
+					<td><?php printf('%d', $narrative['audio_length']/60); ?>:<?php printf('%02d', $narrative['audio_length']%60); ?></td>
+					<td><?php print $narrative['language']; ?></td>
+					<td><?php print $narrative['created']; ?></td>
+					<td><?php print $narrative['uploaded']; ?></td>
+					<td><?php echo anchor('admin/narratives/' . $narrative['narrative_id'] . '/review', $narrative['flags']); ?></td>
+					<td><?php if($narrative['status'] == 1) echo 'Published'; else echo 'Not Published'; ?></td>
+					<td>
+						<?php echo anchor('admin/narratives/' . $narrative['narrative_id'] . '/review', '<span class="glyphicon glyphicon-eye-open"></span>', 'title="Review" class="btn btn-default btn-xs" role="button"'); ?>
+						<?php echo anchor('admin/comments/' . $narrative['narrative_id'], '<span class="glyphicon glyphicon-comment">', 'title="View Comments" class="btn btn-default btn-xs" role="button"'); ?>
+						<?php echo anchor('admin/narratives/' . $narrative['narrative_id'], '<span class="glyphicon glyphicon-pencil"></span>', 'title="Edit" class="btn btn-default btn-xs" role="button"'); ?>
+						<?php echo anchor('admin/narratives/' . $narrative['narrative_id'] . '/delete', '<span class="glyphicon glyphicon-remove">', 'title="Delete" class="btn btn-default btn-xs" role="button"'); ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+	
+	<!-- FIXME should be button anchor links and use JS to trigger submit buttons above -->
+	<?php echo form_open('admin/batchAction'); ?>
+	<ul class="pagination float-left">
+		<li><?php echo form_submit('delete', 'Delete', "class='btn btn-default'"); ?></li>
+		<li><?php echo form_submit('publish', 'Publish', "class='btn btn-default'"); ?></li>
+		<li><?php echo form_submit('unpublish', 'Unpublish', "class='btn btn-default'"); ?></li>
+	</ul>
+	<ul class="pagination float-right">
+		<li><a href="#">&laquo;</a></li>
+		<li><a href="#">1</a></li>
+		<li><a href="#">2</a></li>
+		<li><a href="#">3</a></li>
+		<li><a href="#">4</a></li>
+		<li><a href="#">5</a></li>
+		<li><a href="#">&raquo;</a></li>
+	</ul>
+	<div class="clear"></div>
 
 </div>
--- application/views/include/navigation.php
+++ application/views/include/navigation.php
@@ -9,7 +9,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Admin Panel</a>
+			<?php echo anchor('admin', 'Admin Panel', 'class="navbar-brand"'); ?>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
