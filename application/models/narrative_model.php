faf82f776b1295d6abd2c0ec98f4b2659c08052b
Jad Khoriaty <jadk92@gmail.com>
2014-02-28 17:56:32 -0500

    Fixed merging conflicts

--- application/models/narrative_model.php
+++ application/models/narrative_model.php
@@ -189,21 +189,14 @@ class Narrative_Model extends CI_Model
 		$file_scan = scandir($dir);
 		foreach ($file_scan as $filecheck)
 		{
-<<<<<<< HEAD
 			//Handling upload of preciously downloaded narrative
 			if($filecheck == 'audio_container.txt') unlink($narrative_path . '/audio_container.txt');
 			else if($filecheck == 'AudioTimes.xml') unlink($narrative_path . '/AudioTimes.xml');
 			else if($filecheck == 'combined.mp3') unlink($narrative_path . '/combined.mp3');
 			else
-=======
-			$file_extension = pathinfo($filecheck, PATHINFO_EXTENSION);
-
-			//Handling of batch upload, ignoring directories '.' and '..'
-			if ($file_extension == '' && $filecheck != '.' && $filecheck != '..' && $filecheck != 'deleted')
->>>>>>> f86ce76fb5bae448d5360a0e0dcec92670bd95e7
 			{
 				$file_extension = pathinfo($filecheck, PATHINFO_EXTENSION);
-				
+
 				//Handling of batch upload, ignoring directories '.' and '..'
 				if ($file_extension == '' && $filecheck != '.' && $filecheck != '..' && $filecheck != 'deleted')
 				{
@@ -237,30 +230,6 @@ class Narrative_Model extends CI_Model
 					$narrative_submit_time = $this->get_XML_narrative_submitTime($xml_reader); //check that time format is correct
 					str_replace("-", ":", $narrative_submit_time);
 				}
-<<<<<<< HEAD
-=======
-			}
-			if($this->is_image($file_extension))
-			{
-				$fname = pathinfo($filecheck, PATHINFO_FILENAME);
-				//False variable in process_image controls whether we delete the original image or not (false = do not delete image)
-				$this->process_image($filecheck, $fname, $file_extension, $dir, false);
-
-				//New method, to be approved by TL
-				$images[$fname] = $fname . '.' . $image_format;
-				if($fname > $lastImage) $lastImage = $fname;
-			}
-			if ($file_extension == "xml")
-			{
-				$xmlExistence = TRUE;
-				//read uploaded xml here and hash unique id
-				$xml_reader = simplexml_load_file($dir . "/" . $filecheck);
-				$narrative_name = $this->get_XML_narrative_name($xml_reader); //check if integer, check if RIGHT integer
-				$narrative_language = $this->get_XML_narrative_language($xml_reader); //check if right language (string format)
-				$narrative_submit_date = $this->get_XML_narrative_submitDate($xml_reader); //check if it is a date, check that it is in right format
-				$narrative_submit_time = $this->get_XML_narrative_submitTime($xml_reader); //check that time format is correct
-				str_replace("-", ":", $narrative_submit_time);
->>>>>>> f86ce76fb5bae448d5360a0e0dcec92670bd95e7
 			}
 		}
 		
