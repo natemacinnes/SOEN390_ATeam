fd8bbbe8601cb83239c242df4c17c86ee1d5c8d0
Stewart Adam <s.adam@diffingo.com>
2014-03-08 17:58:47 -0500

    Add RGB color modification script to docs for later use

---
+++ docs/rgb-alter.py
@@ -0,0 +1,9 @@
+def multiply_rgb(color, alter):
+  """Accepts a HTML RGB color and alters each color channel."""
+  rgb = color[1:]
+  chunks, chunk_size = len(rgb), len(rgb)/3
+  r, g, b = [ int(int('0x%s' % rgb[i:i+chunk_size], 0) * alter) for i in range(0, chunks, chunk_size) ]
+  return '#%.2x%.2x%.2x' % (r, g, b)
+
+darken = 0.8
+print multiply_rgb('#ffffff', darken)
