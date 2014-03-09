93086f1f6af96fa06d7cb09f777a7f919c51496b
Sahil N <sahiln25@gmail.com>
2014-03-09 00:44:00 -0500

    Revert "Revert "no message""
    This reverts commit 328d589d6afce8d3a20592e3e0946266f80c3120.

--- tests/models/comment_model_test.php
+++ tests/models/comment_model_test.php
@@ -43,7 +43,7 @@ class Comment_Model_Test extends CIUnit_TestCase
 	 */
 	public function test__get_all__multiple_returned()
 	{
-		$comments = $this->CI->comment_model->get_all();
+		$comments = $this->CI->comment_model->get_all(1);
 
 		$count = 0;
 		$array_only = TRUE;
