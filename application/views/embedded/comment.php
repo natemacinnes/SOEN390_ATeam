d6cbc4a3340c6a3acce8e163f353991025fa17a6
Massimo Catanzariti <mcatanzariti89@gmail.com>
2014-03-13 17:57:27 -0400

    fixing date error when posting comment

--- application/views/embedded/comment.php
+++ application/views/embedded/comment.php
@@ -1,7 +1,14 @@
 <div class="comment" id="comment-<?php echo $comment['comment_id'] ?>">
 	<p><?php echo $comment['body']; ?></p>
   <div class="pull-left">
-	<span class="grey date"><?php echo $comment['created'];?></span>
+	<span class="grey date">
+	<?php 
+		if(array_key_exists("created", $comment))
+		{
+			echo $comment['created'];
+		}
+	?>
+	</span>
   </div>
   <div class="actions">
 	<a class="action-comment-reply" href="#" style="display: none;"><span class="glyphicon glyphicon-comment"></span> Reply</a>
