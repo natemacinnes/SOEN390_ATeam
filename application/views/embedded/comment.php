80dea95d23a1dbb5eaf6cba60e5fe89156b8a689
Massimo Catanzariti <mcatanzariti89@gmail.com>
2014-03-19 20:41:50 -0400

    Starting replying to comment

--- application/views/embedded/comment.php
+++ application/views/embedded/comment.php
@@ -1,5 +1,16 @@
 <div class="comment" id="comment-<?php echo $comment['comment_id'] ?>">
+	<?php if(array_key_exists("parent", $comment)): ?>
+		<p class="quote" id="parent-<?php echo $comment['parent']['id']; ?>"><?php echo $comment['parent']['body']; ?>
+	<?php endif; ?>
 	<p><?php echo $comment['body']; ?></p>
+  <div class="pull-left">
+	<span class="grey date">
+	<?php 
+		if(array_key_exists("created", $comment))
+		{
+			echo $comment['created'];
+		}
+	?>
   <div class="actions">
 	  <a class="action-comment-reply" href="#"><span class="glyphicon glyphicon-comment"></span> Reply</a>
     <a class="action-comment-report" href="#"><span class="glyphicon glyphicon-flag"></span></a>
