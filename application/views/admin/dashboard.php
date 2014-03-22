2f8267b8140c2c6c364cdd966ae6533d6acb8536
Nathan MacInnes <natemacinnes@gmail.com>
2014-03-22 19:53:50 -0400

    Revert "CSS changes to main page, admin dashboard tabs"
    This reverts commit 7ba549b04d2d41516efecaa22103d920c9caf795.

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -3,42 +3,7 @@
 		<h1>Dashboard</h1>
 	</div>
 
-
-
-<div class="tabbable tabs-left">
-        <ul class="nav nav-tabs">
-          <li class="active"><a href="#lA" data-toggle="tab">Narratives</a></li>
-          <li><a href="#lB" data-toggle="tab">Upload</a></li>
-          <li><a href="#lC" data-toggle="tab">Comments</a></li>
-          <li><a href="#lD" data-toggle="tab">Settings</a></li>
-
-        </ul>
-        <div class="tab-content">
-          <div class="tab-pane active" id="lA">
-          	<p>Narratives</p>
-          </div>
-          <div class="tab-pane" id="lB">
-            <p>Upload</p>
-          </div>
-          <div class="tab-pane" id="lC">
-            <p>Comments</p>
-          </div>
-          <div class="tab-pane" id="lD">
-            <p>Settings</p>
-          </div>
-        </div>
-</div>
-
-<!-- <ul class="nav nav-stacked">
-  <li class=""><?php echo anchor('admin/narratives', 'Narratives'); ?></li>
-  <li><?php echo anchor('admin/upload', 'Upload'); ?></a></li>
-  <li><?php echo anchor('admin/comments', 'Comments'); ?></li>
-  <li><?php echo anchor('admin/settings', 'Settings'); ?>
-  </li>
-</ul>
- -->
-
-	<!-- <table class="table">
+	<table class="table">
 		<tbody>
 			<tr>
 				<td><?php echo anchor('admin/narratives', 'Narratives', "class='btn btn-default'"); ?></td>
@@ -47,6 +12,5 @@
 				<td><?php echo anchor('admin/settings', 'Settings', "class='btn btn-default'"); ?></td>
 			</tr>
 		</tbody>
-	</table> -->
-
+	</table>
 </div>
