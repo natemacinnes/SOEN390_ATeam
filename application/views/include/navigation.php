5ee3f736da19700c140caaf3f4c66af00c41a96e
Stewart Adam <s.adam@diffingo.com>
2014-04-03 17:56:41 -0400

    Adjust navigation to fit smaller screens

--- application/views/include/navigation.php
+++ application/views/include/navigation.php
@@ -3,7 +3,7 @@
 	<div class="container">
 
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#yd-navbar">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
@@ -13,18 +13,18 @@
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<div class="collapse navbar-collapse" id="yd-navbar">
 
 			<ul class="nav navbar-nav">
-				<li class="dropdown"><?php echo anchor('admin/narratives', 'Manage Narratives'); ?></li>
-				<li class="dropdown"><?php echo anchor('admin/upload', 'Upload Narratives'); ?></li>
-				<li><?php echo anchor('admin/comments', 'Manage Comments'); ?></li>
+				<li class="dropdown"><?php echo anchor('admin/narratives', 'Narratives'); ?></li>
+				<li class="dropdown"><?php echo anchor('admin/upload', 'Upload'); ?></li>
+				<li><?php echo anchor('admin/comments', 'Comments'); ?></li>
 				<li><?php echo anchor_popup('http://www.google.ca/analytics/', 'Metrics'); ?></li>
 			</ul>
 
 			<ul class="nav navbar-nav navbar-right">
 					<li><?php echo anchor('home', 'Portal Homepage'); ?></li>
-					<li><?php echo anchor('admin/settings', 'Portal settings'); ?></li>
+					<li><?php echo anchor('admin/settings', 'Settings'); ?></li>
 					<?php if ($logged_in_user): ?>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $logged_in_user['login'] ?><b class="caret"></b></a>
