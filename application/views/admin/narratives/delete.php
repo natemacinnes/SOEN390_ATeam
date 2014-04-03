ef0d7eb9994746f07278f63156aa568b45f63179
Stewart Adam <s.adam@diffingo.com>
2014-04-03 17:48:12 -0400

    Change table row hover color to yellowish, change delete color to red

--- application/views/admin/narratives/delete.php
+++ application/views/admin/narratives/delete.php
@@ -31,15 +31,15 @@
 
 		<?php if (count($narratives) == 1): ?>
 			<h3>Narrative deletion:</h3>
-			<p>Deleting a narrative will <strong>irreversibly</strong> delete its voting & shared data. If you wish to retain this information, consider unpublishing instead.</p>
+			<p>Deleting a narrative will <strong>irreversibly</strong> delete its voting &amp; shared data. If you wish to retain this information, consider unpublishing instead.</p>
 			<?php echo anchor('admin/narratives/' . $narratives[0] . '/processDelete', 'Delete', "class='btn btn-default'"); ?>
 			<?php echo anchor('admin', 'Cancel', "class='btn btn-default'"); ?>
 		<?php else: ?>
 			<h3>Narrative deletion:</h3>
-			<p>Deleting a narrative will <strong>irreversibly</strong> delete its voting & shared data. If you wish to retain this information, consider unpublishing instead.</p>
+			<p>Deleting a narrative will <strong>irreversibly</strong> delete its voting &amp; shared data. If you wish to retain this information, consider unpublishing instead.</p>
 			<?php echo form_open('admin/narratives/bulk/delete', 'class="big-bottom-margin"'); ?>
 			<input type='hidden' name='narratives' value="<?php echo htmlentities(serialize($narratives)); ?>" />
-				<?php echo form_submit('submit', 'Delete All', "class='btn btn-default'"); ?>
+				<?php echo form_submit('submit', 'Delete All', "class='btn btn-danger'"); ?>
 				<?php echo anchor('admin', 'Cancel', "class='btn btn-default'"); ?>
 			<?php echo form_close(); ?>
 		<?php endif; ?>
